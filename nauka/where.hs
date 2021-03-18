09205fd20de824c55d5b7d861a554d3fa82df768
Robert Tarnas <robert.tarnas@protonmail.com>
2021-03-18 13:22:03 +0100

    nauka update

--- nauka/where.hs
+++ nauka/where.hs
@@ -9,3 +9,20 @@ mult1   = x * y
 mult2 = x * 3 + y
  where x = 3
        y = 1000
+
+-- let y = 10; x = 10 * 5 + y in x * 5
+
+mult3   = x * 5
+  where y = 10
+        x = 10 * 5 + y
+
+
+-- let x = 7
+-- y = negate x
+--  z = y * 10
+-- in z / x + y
+
+mult4   = z / x + y
+ where y = negate x
+       x = 7
+       z = y * 10
